47b5625582a7b1a15c721702e99bc5038ddd79fe
Alaeddin Dhahri <mr.alaeddindhahri@gmail.com>
2019-11-25 14:07:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Title from './components/AppTitle'
+import SearchBar from './components/SearchBar'
+import MovieCard from './components/MovieCard'
+import stars1 from './img/1star.png'
+import stars2 from './img/2stars.png'
+import stars3 from './img/3stars.png'
+import stars4 from './img/4stars.png'
+import stars5 from './img/5stars.png'
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      moviesList:[{
+        title: 'Joker',
+        release: '2019',
+        imageUrl: 'https://www.joblo.com/assets/images/joblo/posters/2019/08/joker-poster-main2.jpg',
+        ratingUrl: stars5,
+        rating: 5
+      },{
+        title: 'Alita',
+        release: '2019',
+        imageUrl: 'https://i.etsystatic.com/19472203/r/il/e27854/1763716170/il_794xN.1763716170_mzto.jpg',
+        ratingUrl: stars4,
+        rating:4
+      },{
+        title: 'El Camino',
+        release: '2019',
+        imageUrl: 'https://i.redd.it/vxj9weop6hi31.jpg',
+        ratingUrl: stars3,
+        rating:3
+      },{
+        title: 'Midway',
+        release: '2019',
+        imageUrl: 'http://www.impawards.com/2019/posters/midway_xlg.jpg',
+        ratingUrl: stars2,
+        rating: 2
+      }]
+    }
+  }
+  handleSearch=(strSearch,starsSearch,e)=>{
+    e.preventDefault();
+    const tmpMoviesList=this.state.moviesList.filter(strMovie =>strMovie.title===strSearch)
+    console.log(tmpMoviesList)
+    // this.setState({
+
+    // })
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title/>
+      <div className='container'>
+        <SearchBar/>
+        <div className='movies'>
+          {this.state.moviesList.map((el,key)=>
+            <MovieCard myMovie={el} key={key}/>
+          )}
+          <div className="add-movie-card">
+              <a href="">+</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,64 @@
+import React,{Component} from 'react';
+import Star from '../img/star.png';
+
+export default class searchBar extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            searchInput:"",
+            searchStars:[false,false,false,false,false]
+        }
+    }
+    handleStarClick=(starId)=>{
+        let tmpSearchStars = this.state.searchStars
+        // console.log(tmpSearchStars)
+        // disabling the stars behind the clicked star
+        if(tmpSearchStars[starId]===true){
+            for(let i=starId+1;i<=4;i++){
+                tmpSearchStars[i]=false
+            }
+            this.setState({
+                searchStars:tmpSearchStars
+            })
+            return false
+        }
+        //enabling-disabling the stars ahead of the clicked star
+        for(let i=0;i<=starId;i++){
+            tmpSearchStars[i]=!tmpSearchStars[starId]
+        }
+        
+        this.setState({
+            searchStars:tmpSearchStars
+        })
+    }
+    handleMovieTitleChange=(e)=>{
+        this.setState({
+            searchInput:e.target.value
+        })
+    }
+    
+    render(){
+        // console.log(this.state.searchInput)
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-light">
+                <form className="form-inline">
+                    <input type="text" className="search-box" placeholder="search for movies..." onChange={this.handleMovieTitleChange}/>
+                    <button className="btn-primary btn-search" type="submit" onSubmit={()=>this.props.handleSearch(this.state.searchInput,this.state.searchStars)}>Search</button>
+                    <button className="btn-secondary btn-reset" type="reset">Reset</button>
+                    <div className="rating">
+                        <p className="min-rating">Minimum rating</p>
+                        <ul className="search-star">
+                            <li><a href="#" onClick={()=>this.handleStarClick(0)}><img className="star" src={Star} style={{opacity:this.state.searchStars[0]?1:.5}}/></a></li>
+                            <li><a href="#" onClick={()=>this.handleStarClick(1)}><img className="star" src={Star} style={{opacity:this.state.searchStars[1]?1:.5}}/></a></li>
+                            <li><a href="#" onClick={()=>this.handleStarClick(2)}><img className="star" src={Star} style={{opacity:this.state.searchStars[2]?1:.5}}/></a></li>
+                            <li><a href="#" onClick={()=>this.handleStarClick(3)}><img className="star" src={Star} style={{opacity:this.state.searchStars[3]?1:.5}}/></a></li>
+                            <li><a href="#" onClick={()=>this.handleStarClick(4)}><img className="star" src={Star} style={{opacity:this.state.searchStars[4]?1:.5}}/></a></li>
+                        </ul>
+                    </div>
+                </form>
+                </nav>
+        </div>
+    )
+    }
+}
